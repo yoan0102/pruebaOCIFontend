56a56292def2762d57870383834e41beaf18cf62
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2022-10-30 20:22:21 -0400

    fix: refracgtor plantilla

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,17 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
-import { router } from "./constants";
+import { router } from './constants';
 
-import { HomeScreen } from "./pages/home/HomeScreen";
-import { Dashboard } from "./pages/dashboard/Dashboard";
-import { DashboardIndex } from "./pages/dashboard/DashboardIndex";
+import { HomeScreen } from './pages/home';
+import { Dashboard, DashboardIndex } from './pages/dashboard';
 
 const Router = () => {
   return (
     <Routes>
       <Route path={router.HOME} element={<HomeScreen />} />
-      <Route path={router.DASHBOARD} element={<Dashboard />} />
-      <Route path={router.DASHBOARD + "/index"} element={<DashboardIndex />} />
+      <Route path={router.DASHBOARD} element={<DashboardIndex />}>
+        <Route path={router.DASHBOARD} element={<Dashboard />} />
+      </Route>
     </Routes>
   );
 };
